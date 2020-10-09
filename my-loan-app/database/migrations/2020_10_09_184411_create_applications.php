187d96eeba437b72abeae39a7e8bd21fbc8b5f1f
Chris Flora <chrisflora@gmail.com>
2020-10-09 13:24:15 -0700

    Adding some database migrations to the application

---
+++ my-loan-app/database/migrations/2020_10_09_184411_create_applications.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::create('loan_applications', function (Blueprint $table) {
+			$table->increments('id');
+            $table->datetime('deleted_at');
+			$table->timestamps();
+		});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+		Schema::drop('loan_applications');
+    }
+}
---
+++ my-loan-app/database/migrations/2020_10_09_200844_create_borrowers.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBorrowers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::create('borrowers', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('first_name');
+			$table->string('last_name');
+            $table->string('job_title');
+            $table->integer('annual_income');
+            $table->string('bank_account');
+            $table->datetime('deleted_at');
+			$table->timestamps();
+		});
+
+		Schema::create('loan_app_borrowers', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('loan_application_id');
+			$table->integer('borrower_id');
+			$table->datetime('deleted_id');
+			$table->timestamps();
+		});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+		Schema::drop('borrowers');
+		Schema::drop('loan_app_borrowers');
+    }
+}
